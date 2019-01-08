b9305b072622003e482a62f9c34f91e5a9641546
Jon <jonathan.mcneil.90@gmail.com>
2019-01-08 16:43:26 -0700

    added protected routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Switch, Route, Redirect } from "react-router-dom";
+import { withUser } from './context/UserProvider'
 import NavbarComponent from './components/Navbar/Navbar'
 import LogIn from './components/LogIn/LogIn'
 import Register from './components/Register/Register'
@@ -9,10 +11,9 @@ import About from './components/About/About'
 import Cart from './components/Cart/Cart'
 import Footer from './components/Footer/Footer'
 import ProductDetail from './components/Store/ProductDetail'
-import { Switch, Route, Redirect } from "react-router-dom";
-import { withUser } from './context/UserProvider'
 import Calculators from './components/Calculators/Calculators';
 import ContactPage from './components/ContactPage/ContactPage';
+import ProtectedRoute from "./shared/ProtectedRoute"
 
 class App extends Component {
   render() {
@@ -22,16 +23,40 @@ class App extends Component {
         <main>
           <NavbarComponent/>
           <Switch>
-              <Route exact path="/" component={HomePage} />
-              <Route exact path="/store" component={Store} />
-              <Route path="/calculate" component={Calculators} />
-              <Route path="/about" component={About} />
-              <Route path="/login" render={routerProps => token ? <Redirect to="/"/> : <LogIn {...routerProps}/>} />
-              <Route path="/register" render={routerProps => token ? <Redirect to="/"/> : <Register {...routerProps}/>} />
-              <Route path="/profile" component={Profile} />
-              <Route path="/cart" component={Cart} />
-              <Route path='/contact' component={ContactPage} />
-              <Route exacpt path="/store/:id" component={ProductDetail} />
+              <Route 
+                exact path="/" 
+                component={HomePage} />
+              <Route 
+                exact path="/store" 
+                component={Store} />
+              <Route 
+                path="/calculate" 
+                component={Calculators} />
+              <Route 
+                path="/about" 
+                component={About} />
+              <Route 
+                path="/login" 
+                render={routerProps => token ? <Redirect to="/"/> : <LogIn {...routerProps}/>} />
+              <Route 
+                path="/register" 
+                render={routerProps => token ? <Redirect to="/"/> : <Register {...routerProps}/>} />
+              <ProtectedRoute 
+                path="/profile" 
+                component={Profile} 
+                redirectTo={'/'} 
+                token={token} />
+              <ProtectedRoute 
+                path="/cart" 
+                component={Cart}
+                redirectTo={'/'} 
+                token={token}/>
+              <Route 
+                path='/contact' 
+                component={ContactPage} />
+              <Route 
+                exacpt path="/store/:id" 
+                component={ProductDetail} />
           </Switch>
         </main>
         <Footer />
