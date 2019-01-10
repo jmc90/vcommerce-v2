2616adab25e53bc6c2fb1d6e5bdca8e96afd0919
Jon <jonathan.mcneil.90@gmail.com>
2019-01-10 09:44:55 -0700

    edit user info

--- client/src/context/UserProvider.js
+++ client/src/context/UserProvider.js
@@ -1,103 +1,121 @@
-import React, { Component } from 'react'
-import axios from 'axios'
+import React, { Component } from "react";
+import axios from "axios";
 const authAxios = axios.create();
 
-authAxios.interceptors.request.use((config)=>{
-    const token = localStorage.getItem("token");
-    config.headers.Authorization = `Bearer ${token}`;
-    return config;
-})
+authAxios.interceptors.request.use(config => {
+  const token = localStorage.getItem("token");
+  config.headers.Authorization = `Bearer ${token}`;
+  return config;
+});
 
-const UserContext = React.createContext()
+const UserContext = React.createContext();
 
 class UserProvider extends Component {
   constructor() {
-    super()
+    super();
     this.state = {
       user: JSON.parse(localStorage.getItem("user")) || {},
       token: localStorage.getItem("token") || "",
       logInErrorMessage: "",
       registerErrorMessage: ""
-    }
+    };
   }
 
   register = userInfo => {
-    axios.post('/auth/register', userInfo).then(res => {
-       const { user, token } = res.data
-       localStorage.setItem("user", JSON.stringify(user))
-       localStorage.setItem("token", token)
-       this.setState({
-         user: user,
-         token: token,
-         logInErrorMessage: "",
-         registerErrorMessage: ""
-       })
-     })
-     .catch(err => this.handleError(err.response.data.errMsg, 'register'))
-   }
-
-   logIn = userInfo => {
-    axios.post('/auth/login', userInfo).then(res => {
-      console.log(res.data)
-      const { token, user } = res.data
-      localStorage.setItem("token", token)
-      localStorage.setItem("user", JSON.stringify(user))
-      this.setState({
-        user: user,
-        token: token,
-        logInErrorMessage: "",
-        registerErrorMessage: ""
+    axios
+      .post("/auth/register", userInfo)
+      .then(res => {
+        const { user, token } = res.data;
+        localStorage.setItem("user", JSON.stringify(user));
+        localStorage.setItem("token", token);
+        this.setState({
+          user: user,
+          token: token,
+          logInErrorMessage: "",
+          registerErrorMessage: ""
+        });
       })
-    })
-    .catch(err => {
-      this.handleError(err.response.data.errMsg, 'logIn')
-    })
-  }
+      .catch(err => this.handleError(err.response.data.errMsg, "register"));
+  };
 
-   handleError = (err, errRoute) => {
-    if (errRoute === 'logIn') {
+  logIn = userInfo => {
+    axios
+      .post("/auth/login", userInfo)
+      .then(res => {
+        console.log(res.data);
+        const { token, user } = res.data;
+        localStorage.setItem("token", token);
+        localStorage.setItem("user", JSON.stringify(user));
+        this.setState({
+          user: user,
+          token: token,
+          logInErrorMessage: "",
+          registerErrorMessage: ""
+        });
+      })
+      .catch(err => {
+        this.handleError(err.response.data.errMsg, "logIn");
+      });
+  };
+
+  handleError = (err, errRoute) => {
+    if (errRoute === "logIn") {
       this.setState({
         logInErrorMessage: err
-    })
-    } else if (errRoute === 'register') {
+      });
+    } else if (errRoute === "register") {
       this.setState({
         registerErrorMessage: err
-      })
+      });
     }
-  }
+  };
 
   logOut = () => {
     localStorage.removeItem("user");
     localStorage.removeItem("token");
     this.setState({
-        entries: [],
-        singleEntry: {},
-        user: {},
-        token: ""
-    })
-  }
+      entries: [],
+      singleEntry: {},
+      user: {},
+      token: ""
+    });
+  };
 
   addToWishList = (sku, quantity) => {
-    authAxios.put(`/api/wishlist/add/${sku}`, {quantity}).then(res =>{
-      localStorage.setItem("user", JSON.stringify(res.data))
-      this.setState({
-        user: res.data
+    authAxios
+      .put(`/api/wishlist/add/${sku}`, { quantity })
+      .then(res => {
+        localStorage.setItem("user", JSON.stringify(res.data));
+        this.setState({
+          user: res.data
+        });
       })
-    })
-    .catch(err => console.log(err))
-  }
+      .catch(err => console.log(err));
+  };
 
   addToCart = (sku, quantity) => {
-    authAxios.put(`/api/cart/add/${sku}`, {quantity}).then(res =>{
+    authAxios
+      .put(`/api/cart/add/${sku}`, { quantity })
+      .then(res => {
+        console.log(res);
+        localStorage.setItem("user", JSON.stringify(res.data));
+        this.setState({
+          user: res.data
+        })
+      })
+      .catch(err => console.log(err));
+  }
+
+  handleUserInfoEdit = updates => {
+    axios.put(`/api/user/userinfo`, updates).then(res => {
       console.log(res)
-      localStorage.setItem("user", JSON.stringify(res.data))
+      localStorage.setItem("user", JSON.stringify(res.data));
       this.setState({
         user: res.data
       })
     })
     .catch(err => console.log(err))
   }
- 
 
   render() {
     return (
@@ -110,19 +128,18 @@ class UserProvider extends Component {
           user: this.state.user,
           addToWishList: this.addToWishList,
           addToCart: this.addToCart
-
-        }}>
+        }}
+      >
         {this.props.children}
       </UserContext.Provider>
-    )
+    );
   }
 }
 
-export default UserProvider
-
+export default UserProvider;
 
 export const withUser = C => props => (
   <UserContext.Consumer>
     {value => <C {...props} {...value} />}
   </UserContext.Consumer>
-)
+);
